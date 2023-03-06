3f2bb0130ee0633bd7d3fdaef3fdc6e362b27eb4
Hoytmelcher <122131937+Hoytmelcher@users.noreply.github.com>
2023-03-06 14:44:09 -0600

    revert before host

--- app/__init__.py
+++ app/__init__.py
@@ -6,31 +6,32 @@ from flask_login import LoginManager
 from flask_cors import CORS
 
 
+
+
+
+app = Flask(__name__)
+CORS(app)
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-    app.config.from_object(Config)
+login.login_view='/login'
+login.login_message="Make sure you log in"
 
-    login.login_view='/login'
-    login.login_message="Make sure you log in"
+from app.blueprints.auth import bp as auth_bp
+app.register_blueprint(auth_bp)
 
-    from app.blueprints.auth import bp as auth_bp
-    app.register_blueprint(auth_bp)
+from app.blueprints.main import bp as main_bp
+app.register_blueprint(main_bp)
 
-    from app.blueprints.main import bp as main_bp
-    app.register_blueprint(main_bp)
+from app.blueprints.user import bp as social_bp
+app.register_blueprint(social_bp)
 
-    from app.blueprints.user import bp as social_bp
-    app.register_blueprint(social_bp)
+from app.blueprints.api import bp as api_bp
+app.register_blueprint(api_bp)
 
-    from app.blueprints.api import bp as api_bp
-    app.register_blueprint(api_bp)
+from app.blueprints.pokemon import bp as poke_bp
+app.register_blueprint(poke_bp)
 
-    from app.blueprints.pokemon import bp as poke_bp
-    app.register_blueprint(poke_bp)
-
-    return app
+   
